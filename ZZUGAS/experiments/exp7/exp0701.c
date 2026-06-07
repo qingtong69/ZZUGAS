5f521e3f793832d74ab6d6b60ff64ec7f69beae3
qingtong69 <qingtong69@qq.com>
2026-06-07 14:31:49 +0800

    Add binary print functionality for EAX register

--- ZZUGAS/experiments/exp7/exp0701.c
+++ ZZUGAS/experiments/exp7/exp0701.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 void printEAX() {
-    
+    int i;
+    for (i = 31; i >= 0; i--)
+    {
+        if (val & (1U << i))
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
 }
 
 int main() {
